8e9596f36260ecc665e46b321d4744f8b30fbae3
Michał Sikorski <37452425+yourbestnightmare@users.noreply.github.com>
2018-03-24 10:49:15 +0100

    Teraz odczytuje te dwie liczby z konsoli.

--- Kalkulator.cs
+++ Kalkulator.cs
@@ -1,11 +1,20 @@
 using System;
-
+ 
 public class Test
 {
 	public static void Main()
 	{
-		int a = 5;
-		int b = 5;
-		Console.WriteLine(a + b);
+		
+		int a, b, wynik;
+ 
+     		Console.Write("Liczba 1: ");
+     		a = int.Parse(Console.ReadLine());
+ 
+    		Console.Write("Liczba 2: ");
+     		b = int.Parse(Console.ReadLine());
+ 
+     		wynik = a + b;
+     		Console.WriteLine("Wynik: " + wynik);
+		
 	}
 }
